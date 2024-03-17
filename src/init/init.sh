fcc4f3401b6932293e390b5c3a956b1868f6c3b3
Mehver | 这水怎么没味儿啊 <75297777+Mehver@users.noreply.github.com>
2024-03-16 23:32:25 -0400

    Update init.sh

--- src/init/init.sh
+++ src/init/init.sh
@@ -1,17 +1,17 @@
 if [ ! -f /usr/lib/timetender/config/timetender.json ]; then
-  cp /usr/lib/timetender/bin/init/config/timetender.json /usr/lib/timetender/config/timetender.json
+  cp /usr/lib/timetender/src/init/config/timetender.json /usr/lib/timetender/config/timetender.json
 fi
 if [ ! -f /usr/lib/timetender/data/data.json ]; then
-  cp /usr/lib/timetender/bin/init/data/data.json /usr/lib/timetender/data/data.json
+  cp /usr/lib/timetender/src/init/data/data.json /usr/lib/timetender/data/data.json
 fi
 if [ ! -f /usr/lib/timetender/data/data_auto-save.json ]; then
-  cp /usr/lib/timetender/bin/init/data/data_auto-save.json /usr/lib/timetender/data/data_auto-save.json
+  cp /usr/lib/timetender/src/init/data/data_auto-save.json /usr/lib/timetender/data/data_auto-save.json
 fi
 if [ ! -f /usr/lib/timetender/data/event.json ]; then
-  cp /usr/lib/timetender/bin/init/data/event.json /usr/lib/timetender/data/event.json
+  cp /usr/lib/timetender/src/init/data/event.json /usr/lib/timetender/data/event.json
 fi
 if [ ! -f /usr/lib/timetender/data/tag.json ]; then
-  cp /usr/lib/timetender/bin/init/data/tag.json /usr/lib/timetender/data/tag.json
+  cp /usr/lib/timetender/src/init/data/tag.json /usr/lib/timetender/data/tag.json
 fi
 node /usr/lib/timetender/server.js
 
